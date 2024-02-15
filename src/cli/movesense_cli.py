82da355bcb43b0d2f4dd1bb80348a5bb9e684678
Henry Mauranen <henry.mauranen@aalto.fi>
2024-02-15 13:19:59 +0200

    Initial commit

---
+++ src/cli/movesense_cli.py
@@ -0,0 +1,191 @@
+import sys
+import re
+import asyncio
+
+from src.movesense.movesense_device_manager import MovesenseDeviceManager
+import logging
+logger = logging.getLogger("__main__")
+
+
+class MovesenseCLI:
+    def __init__(self, config=None):
+        self.config = config or {}
+
+        self.device_manager = MovesenseDeviceManager(config)
+        self.output_filename = None
+
+    def display_menu(self):
+        logger.info("==== Movesense CLI Menu ====")
+        logger.info("1. Start Data Collection")
+        logger.info("2. Connect to an Additional Device")
+        logger.info("3. Configure Connected Devices")
+        logger.info("4. Save Current Configuration")
+        logger.info("5. Exit")
+
+    def start_device_connection_activity(self):
+        found_devices = self.device_manager.get_available_devices()
+
+        if not found_devices:
+            logger.warning("No MoveSense devices found. Make sure that the MoveSense devices are turned on, and not "
+                        "connected to another device.")
+            return
+        else:
+            logger.info("Select the MoveSense Device to connect to (list-id or mac-address)")
+            choice = input()
+
+            # Regex match for MAC address input (MAC address regex)
+            if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", choice.lower()):
+                # Find the device with the specified MAC address
+                selected_device = next((device for device in found_devices if device.device.address.lower() == choice.lower()),
+                                       None)
+
+                if selected_device:
+                    try:
+                        self.device_manager.connect(selected_device)
+                        if "devices" in self.config:
+                            self.config["devices"].append({"address": selected_device, "paths": []})
+                        else:
+                            self.config["devices"] = [{"address": selected_device, "paths": []}]
+                        logger.info(f"Connected to device with MAC address: {selected_device.device.device.address}")
+                    except Exception as e:
+                        logger.error(f"Failed to connect to device with MAC address '{selected_device.device.device.address}': {e}")
+                else:
+                    logger.warning(f"No device found with MAC address '{choice}'")
+
+            else:
+                try:
+                    index = int(choice) - 1
+                    self.device_manager.connect(found_devices[index])
+                    if "devices" in self.config:
+                        self.config["devices"].append({"address": found_devices[index].address, "paths": []})
+                    else:
+                        self.config["devices"] = [{"address": found_devices[index].address, "paths": []}]
+
+                    logger.info(f"Connected to device with list-id: {index + 1}, and MAC address: {found_devices[index].address}")
+                except ValueError:
+                    logger.error(f"Invalid input. Please enter a valid list-id as an integer or a MAC address.")
+                except IndexError:
+                    logger.error(f"Invalid list-id '{index}'. List-id out of range.")
+                except Exception as e:
+                    logger.error(f"Unknown device-id '{choice}'. Failed to connect: {e}")
+
+    def start_device_configuration_activity(self):
+
+        if not self.device_manager.connected_devices:
+            logger.warning("No connected devices found")
+            return
+
+        try:
+            while True:
+                logger.info("Select the device to configure. 'return' to exit back to main menu.")
+                self.device_manager.show_connected_devices()
+
+                choice = input()
+
+                if choice == "return":
+                    raise KeyboardInterrupt
+                selected_device = None
+                # If choice based on MAC (MAC address regex)
+                if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", choice.lower()):
+                    selected_device = next((device for device in self.device_manager.connected_devices
+                                            if device.device.address.lower() == choice.lower()),
+                                           None)
+                # If choice based on list id
+                else:
+                    try:
+                        index = int(choice) - 1
+                        selected_device = self.device_manager.connected_devices[index]
+                    except ValueError:
+                        logger.error(f"Invalid input. Please enter a valid list-id as an integer or a MAC address.")
+                    except IndexError:
+                        logger.error(f"Invalid list-id '{index + 1}'. List-id out of range.")
+
+                # Show configuration menu
+                self.start_single_device_configuration(selected_device)
+
+        except KeyboardInterrupt:
+            logger.info("Exiting the device menu")
+
+    def start_single_device_configuration(self, device):
+        try:
+            while True:
+                logger.info("What would you like to do?")
+                logger.info("1. Rename the device")
+                logger.info("2. Subscribe to Acceleration")
+                logger.info("3. Subscribe to Gyroscope")
+                logger.info("4. Subscribe to Magnetometer")
+                logger.info("5. Subscribe to Temperature")
+                logger.info("7. Subscribe to ECG")
+                logger.info("8. Return")
+
+                choice = input()
+
+                if choice == "1":
+                    logger.info("Rename the device")
+                    choice = input("New device name:")
+                    self.device_manager.rename_device(device, choice)
+                elif choice == "2" or choice == "3" or choice == "4" or choice == "5" or choice == "6" or choice == "7":
+                    sensor_full = ["Acceleration", "Gyroscope", "Magnetometer", "Temperature", "ECG"][int(choice) - 2]
+                    sensor_abbreviation = ["Accel", "Gyro", "Magn", "Temperature", "ECG"][int(choice)]
+                    msg = "Subscribe to " + sensor_full
+                    logger.info(msg)
+
+                    try:
+                        logger.info("Choose the sampling rate (13, 26, 52, 104, 208, 416, 833, 1666")
+                        fs = int(input())
+                        # Subscription path determines response id (fixed for ease of use), "Meas", the sensor type,
+                        # and sampling rate
+                        self.config["devices"][device.address]["paths"].append(f"Meas/{sensor_abbreviation}/{fs}")
+                        self.device_manager.subscribe_to_sensor(device, bytearray([1, 99-int(choice)+2]) +
+                                                                bytearray(f"Meas/{sensor_abbreviation}/{fs}", "utf-8"))
+                    except ValueError:
+                        logger.error(f"Invalid input. Please enter a valid integer choice.")
+                elif choice == "8":
+                    raise KeyboardInterrupt
+
+        except KeyboardInterrupt:
+            logger.info("Exiting the device configuration")
+
+
+    def start_collection_activity(self):
+        logger.info("Starting data collection. Press x to terminate")
+        self.device_manager.start_data_collection_sync()
+
+        try:
+            asyncio.get_event_loop().run_until_complete(asyncio.Event().wait())  # Wait indefinitely until the event is set
+        except KeyboardInterrupt:
+            logger.info("Ending data collection, saving...")
+        finally:
+            self.device_manager.end_data_collection()
+
+    def run(self):
+
+        # Loop for running the interface
+        while True:
+            self.display_menu()
+            choice = input()
+
+            if choice == "1":
+                self.start_collection_activity()
+            elif choice == "2":
+                self.start_device_connection_activity()
+            elif choice == "3":
+                self.start_device_configuration_activity()
+            elif choice == "4":
+                pass
+            elif choice == "5" or choice.lower() == "exit":
+                logger.info("Exiting MoveSense CLI. Goodbye.")
+                self.device_manager.disconnect_devices()
+                break
+            else:
+                logger.warning(f"Unrecognized selection {choice}")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    movesense_cli = MovesenseCLI()
+    movesense_cli.run()
---
+++ src/movesense/movesense_device_manager.py
@@ -0,0 +1,145 @@
+from bleak import BleakScanner, BleakClient
+import asyncio
+import logging
+logger = logging.getLogger("__main__")
+
+from bleak.backends.device import BLEDevice
+
+from src.movesense.movesense_data_collector import MovesenseDataCollector
+
+
+WRITE_CHARACTERISTIC = "34800001-7185-4d5d-b431-630e7050e8f0"
+NOTIFY_CHARACTERISTIC = "34800002-7185-4d5d-b431-630e7050e8f0"
+NAME_CHARACTERISTIC = ""
+
+
+class ConnectedDevice:
+    def __init__(self, device: BLEDevice, client: BleakClient):
+        self.device = device
+        self.client = client
+
+
+class MovesenseDeviceManager:
+    def __init__(self, config=None):
+
+        self.data_collector = MovesenseDataCollector()
+
+        self.loop = asyncio.get_event_loop()  # Create an event loop
+        self.connected_devices = []
+
+        # If we have a predefined session config, we utilize it
+        if config:
+            self.config = config
+            # Collect the connectable devices
+            available_devices = self.get_available_devices()
+
+            for d in config["devices"]:
+                if d["address"] not in [device.address for device in available_devices]:
+                    logger.warning(f"Could not connect to device: {d['address']}")
+                else:
+                    # Connect to device
+                    target_device = next((device for device in available_devices if device.address == d["address"]), None)
+                    connected_device = self.connect(target_device)
+                    # Subscribe to the data channels
+                    for path in d["paths"]:
+                        # Add ids to the paths so they can be recognized at read time
+                        id_bytes = bytearray([1, 99])
+                        if "Acc" in path:
+                            id_bytes[1] = id_bytes[1] - 0
+                        elif "Gyro" in path:
+                            id_bytes[1] = id_bytes[1] - 1
+                        elif "Magn" in path:
+                            id_bytes[1] = id_bytes[1] - 2
+                        elif "Temperature" in path:
+                            id_bytes[1] = id_bytes[1] - 3
+                        elif "ECG" in path:
+                            id_bytes[1] = id_bytes[1] - 4
+
+                        byte_path = id_bytes + bytearray(path, "utf-8")
+
+                        self.subscribe_to_sensor(connected_device, byte_path)
+            logger.info("Session config loaded.")
+        else:
+            logger.info("No session config found.")
+
+
+    def run_coroutine_sync(self, coroutine):
+        return self.loop.run_until_complete(coroutine)
+
+    def get_available_devices(self, show_all=False):
+        logger.info("Searching for available devices...")
+        devices = self.run_coroutine_sync(BleakScanner.discover(timeout=5.0))
+        found_devices = []
+        for device in devices:
+            if show_all or "movesense" in device.name.lower():
+                found_devices.append(device)
+                logger.info(f"Found device: {len(found_devices)}. {device.name} - {device.address}")
+
+        return found_devices
+
+    def connect(self, device):
+        async def connection_coroutine(device):
+            client = BleakClient(device.address)
+            await client.connect()
+            logger.info(f"Connected to {device.name} ({device.address})")
+            connected_device = ConnectedDevice(device, client)
+            self.connected_devices.append(connected_device)
+            return connected_device
+
+        return self.run_coroutine_sync(connection_coroutine(device))
+
+    def show_connected_devices(self):
+        logger.info("Connected MoveSense devices:")
+        for i, device in enumerate(self.connected_devices):
+            logger.info(f"{i + 1}. {device.device.name}: {device.device.address}")
+
+    def rename_device(self, device, new_name):
+        async def rename_coroutine(device, new_name):
+            await device.client.write_gatt_char(NAME_CHARACTERISTIC, new_name, response=True)
+
+        self.run_coroutine_sync(rename_coroutine(device, new_name))
+
+    def subscribe_to_sensor(self, device, path):
+        async def subscribe_coroutine(device, path):
+            await device.client.write_gatt_char(WRITE_CHARACTERISTIC, path, response=True)
+        self.run_coroutine_sync(subscribe_coroutine(device, path))
+
+    def start_data_collection_sync(self):
+        logger.debug("Enabling notifications.")
+        # Subscribe to notify for all connected devices
+        for device in self.connected_devices:
+            self.loop.run_until_complete(self.start_notify_coroutine(device))
+        logger.debug("Data collection started.")
+
+    async def start_notify_coroutine(self, device):
+        await device.client.start_notify(NOTIFY_CHARACTERISTIC, lambda sender, data: asyncio.ensure_future(
+            self.data_collector.handle_notification(device.device.address, data)))
+
+    def end_data_collection(self):
+        logger.debug("Disabling notifications.")
+
+        for device in self.connected_devices:
+            self.run_coroutine_sync(device.client.stop_notify(NOTIFY_CHARACTERISTIC))
+
+        # Save the collected data
+        self.data_collector.unify_notifications().to_csv("./data_output.csv", sep=",", index=False)
+
+        logger.info("Data collection complete.")
+
+    def disconnect_device(self, device_id):
+        async def disconnect_coroutine(device_id):
+            device = self.connected_devices[device_id]
+            logger.info(f"Disconnecting device {device.device.name} ({device.device.address})")
+            await device.client.disconnect()
+            self.connected_devices.remove(device)
+
+        # Run disconnection coroutine
+        self.run_coroutine_sync(disconnect_coroutine(device_id))
+
+    def disconnect_devices(self):
+        logger.info("Disconnecting from all MoveSense devices...")
+
+        # Run disconnection coroutines. Repeatedly disconnecting 1st entry since the devices are always popped out of
+        # connected devices.
+        disconnect_coroutines = [self.disconnect_device(0) for _ in range(len(self.connected_devices))]
+        self.run_coroutine_sync(asyncio.gather(*disconnect_coroutines))
